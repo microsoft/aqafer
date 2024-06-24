9d25bfa35374494e8ba024fbb465ef409804e922
joe-braley <earlejoebraley@gmail.com>
2024-06-24 14:05:41 -0700

    Test using Joe's fork
    Swap adoptium branch
    
    add code to clean workspace
    
    fix clean workspace
    
    add version
    
    fix container registry
    
    add in ant install
    
    add in ant install
    
    Update docker workflow
    
    Update to docker workflow
    
    fix typo
    
    Add timeout minutes; fix line endings
    
    bump timeouts; agent pool for docker
    
    Fix line endings
    
    Microsoft mandatory file
    
    Update paralleltest-docker.yml
    
    Update paralleltest.yml
    
    Update aqa.yml
    
    add jdk21
    
    Updated parallel-test run name
    
    revisions
    
    add checkout
    
    move files
    
    fix input type
    
    test other script
    
    test download
    
    add checkout
    
    test
    
    add bash loop
    
    testing
    
    add token read
    
    move token perms
    
    add auth mode login
    
    mkdir
    
    ammend script
    
    print debug
    
    simply if
    
    more print debugs
    
    test
    
    rework python func
    
    add new line for env vars
    
    create parallellist
    
    run functional test
    
    wip
    
    add args for py script
    
    enable workflow
    
    move checkout up
    
    cleanup and pwsh to download win
    
    clean up
    
    fix pwsh
    
    pwsh
    
    newline

---
+++ scripts/vendors/microsoft/configure.py
@@ -0,0 +1,36 @@
+import os
+import json
+import argparse
+
+def main(github_pool, num_lists):
+  github_runner = None
+  test_lists = []
+  platforms = ["windows-x64", "windows-arm64", "linux-x64", "linux-arm64", "macos-x64", "macos-arm64"]
+
+  for platform in platforms:
+    if platform in github_pool:
+      if "macos" in github_pool and "arm64" in github_pool:
+        github_runner = "config=['self-hosted', 'macOS', 'ARM64']"
+      elif "macos" in github_pool and "x64" in github_pool:
+        github_runner = "config='macos-11'"
+      else:
+        github_runner = f"config=['self-hosted', '1ES.Pool={ github_pool }']"
+
+  with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+    print(github_runner, file=fh)
+
+  for list in range(num_lists):
+    test_lists.append(f"testList_{list}")
+
+  with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+    print(f"testConfig={json.dumps(test_lists)}", file=fh)
+
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--pool', type=str, help='Pool to run tests on')
+  parser.add_argument('--testLists', type=int, help='Number of test lists')
+  args = parser.parse_args()
+
+  num_lists = args.testLists
+  main(args.pool, num_lists)
---
+++ scripts/vendors/microsoft/setup_environment.py
@@ -0,0 +1,68 @@
+import argparse
+import tarfile
+import zipfile
+import os
+from pathlib import Path
+from platform import system
+
+def get_test_resources_path(destination) -> str:
+    child_directories = [child for child in destination.iterdir() if child.is_dir()]
+    test_jdk_home = None
+    testimage_path = None
+
+    # should only be two directories to check for
+    for child in child_directories:
+        if "-test-image" in child.name :
+            testimage_path = str(child)
+        elif "-test-image" not in child.name:
+            test_jdk_home = str(child)
+
+    return test_jdk_home, testimage_path
+
+def expand_zip_archive(zip_archive, destination) -> None:
+    if zipfile.is_zipfile(zip_archive):
+        with zipfile.ZipFile(zip_archive) as zip:
+            zip.extractall(destination)
+    else:
+        raise Exception(f"{zip_archive} is not a valid zip archive")
+
+def expand_tar_archive(tar_archive, destination) -> None:
+    if tarfile.is_tarfile(tar_archive):
+        with tarfile.open(tar_archive) as tar:
+            tar.extractall(destination)
+    else:
+        raise Exception(f"{tar_archive} is not a valid tar archive")
+
+def set_openjdk_environment_variables(env_var_name, env_value) -> None:
+    github_env = os.getenv("GITHUB_ENV")
+
+    with open(github_env, 'a') as file:
+        file.write(f"{ env_var_name }={ env_value }\n")
+
+def main(source: Path, destination: Path) -> None:
+    env_vars = {
+        "TEST_JDK_HOME": None,
+        "TESTIMAGE_PATH": None,
+    }
+
+    child_directories = [child for child in source.iterdir()]
+
+    for child in child_directories:
+        if system() == "Windows":
+            expand_zip_archive(child, destination)
+        else:
+            expand_tar_archive(child, destination)
+
+    env_vars["TEST_JDK_HOME"], env_vars["TESTIMAGE_PATH"] = get_test_resources_path(destination)
+    set_openjdk_environment_variables("TEST_JDK_HOME", env_vars["TEST_JDK_HOME"])
+    set_openjdk_environment_variables("TESTIMAGE_PATH", env_vars["TESTIMAGE_PATH"])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source', type=str, help='Path to the archive to extract')
+    parser.add_argument('--destination', type=str, help='Destination directory for the extracted archive')
+    args = parser.parse_args()
+
+    source = Path(args.source)
+    destination = Path(args.destination)
+    main(source, destination)
