d5e6f5b8be674a3367d5d117f6167504a1b6b8e0
joe-braley <earlejoebraley@gmail.com>
2024-06-25 10:47:51 -0700

    clean up py script

--- scripts/vendors/microsoft/configure.py
+++ scripts/vendors/microsoft/configure.py
@@ -2,22 +2,26 @@ import os
 import json
 import argparse
 
+def set_number_of_test_lists(num_lists: int) -> str:
+    return [f"testList_{list}" for list in range(num_lists)]
+
+def get_runner_config(pool_name: str) -> str:
+    platforms = ["windows-x64", "windows-arm64", "linux-x64", "linux-arm64", "macos-x64", "macos-arm64"]
+    for platform in platforms:
+        if platform in pool_name:
+            if "macos" in pool_name and "arm64" in pool_name:
+                return "config='macos-14'"
+            elif "macos" in pool_name and "x64" in pool_name:
+                return "config='macos-11'"
+            else:
+                return f"config=['self-hosted', '1ES.Pool={ pool_name }']"
+
 def main(github_pool, num_lists):
   github_runner = None
   test_lists = []
-  platforms = ["windows-x64", "windows-arm64", "linux-x64", "linux-arm64", "macos-x64", "macos-arm64"]
-  pool_name = github_pool.lower()
 
-  for platform in platforms:
-    if platform in pool_name:
-      if "macos" in pool_name and "arm64" in pool_name:
-        github_runner = "config='macos-14'"
-      elif "macos" in pool_name and "x64" in pool_name:
-        github_runner = "config='macos-11'"
-      else:
-        github_runner = f"config=['self-hosted', '1ES.Pool={ github_pool }']"
-
-  test_lists = [f"testList_{list}" for list in range(num_lists)]
+  github_runner = get_runner_config(github_pool.lower())
+  test_lists = set_number_of_test_lists(num_lists)
 
   with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
     print(github_runner, file=fh)
